f2c85d1cf6f5c7cb3579d2ada18b138764f737e0
yiyinyinguu <yingluu@zju.edu.cn>
2024-06-05 20:49:29 +0000

    add output pdf

--- run.sh
+++ run.sh
@@ -13,3 +13,8 @@ chl_analysis.plot_chl_clim()
 chl_analysis.plot_hatched_chl_trend()
 EOF
 
+cd figs
+mv OC-CCI_Chl_clim_v42_1998-2018_nipy.pdf ../../Fig_2_31a.pdf
+mv OC-CCI_Chl_trend_v42_1998-2018_hatch0o1.pdf ../../Fig_2_31b.pdf
+
+
